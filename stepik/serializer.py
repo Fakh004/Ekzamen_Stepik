7f3ddc6313afb0a2b4e36fbc033c6883722a3021
fakh <fakhridin008@gmail.com>
2026-01-19 01:21:29 +0300

    serializers stepik and courselistCreateView

--- stepik/serializer.py
+++ stepik/serializer.py
@@ -1,4 +1,46 @@
-# from rest_framework import serializers
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Course, Enrollment, Module, Task, InputOutput, Submission
 
-# class StepikSerializer(serializers.Serializer):
-    
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ("id", "title", "author", "created_at", "is_active")
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = ("id", "user", "course")
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ("id", "course", "title", "is_active")
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ("id", "module", "title", "order", "task_text")
+
+
+class InputOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InputOutput
+        fields = ("id", "task", "input", "output")
+
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Submission
+        fields = ("id", "user", "task", "code_student", "status", "created_at")
+        read_only_fields = ("status", "created_at")
--- stepik/views.py
+++ stepik/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Course, Enrollment, Module, Task, InputOutput, Submission
+from .serializer import *
 
-# Create your views here.
+class CourseListCreateView(generics.ListCreateAPIView):
+    queryset = Course.objects.filter(is_active=True)
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
